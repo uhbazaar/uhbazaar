ce6677b70d2d11afea0ee38695cda18a1210171e
Wyatt Hoodes <hoodesw@hawaii.edu>
2018-11-06 21:17:05 -1000

    First draft of user profile finished

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { Container, Divider } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = {
+      backgroundColor: '#17252a',
+      position: 'absolute',
+      bottom: '0',
+      width: '100%',
+      height: '100px',
+    };
     return (
-        <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822
-          </div>
-        </footer>
+        <Container fluid style={divStyle}>
+          <footer>
+            <div className="ui center aligned container">
+              <Container>
+                <Divider horizontal inverted>
+                  UH Bazaar
+                </Divider>
+                <Divider horizontal inverted>
+                  Where East Meets West
+                </Divider>
+              </Container>
+            </div>
+          </footer>
+        </Container>
     );
   }
 }
--- app/imports/ui/pages/UserProfile.jsx
+++ app/imports/ui/pages/UserProfile.jsx
@@ -4,36 +4,42 @@ import { Card, Image, Icon, Container, Grid } from 'semantic-ui-react';
 export default class UserProfile extends React.Component {
   render() {
     const cardStyle = { color: '#17252a' };
+    const gridStyle = { marginTop: '128px' };
     return (
-        <Container fluid className='background-color'>
-          <style>{'body { background-color: #3aafa9; }'}</style>
-          <Grid verticalAlign='middle' className='background-color'>
-            <Grid.Row>
-              <Grid.Column width={8}>
-                <Image size='huge' floated='left' src='/images/tommy-boy.jpg'/>
-              </Grid.Column>
-              <Grid.Column width={8}>
-                <Card floated='right' fluid>
-                  <Card.Content>
-                    <Card.Header style={cardStyle}>Tommy</Card.Header>
-                    <Card.Meta style={cardStyle}>
-                      Joined in 2016
-                    </Card.Meta>
-                    <Card.Description style={cardStyle}>
-                      Tommy is a comedian living in Nashville.
-                    </Card.Description>
-                  </Card.Content>
-                  <Card.Content extra>
-                    <a>
-                      <Icon name='gem'/>
-                      10 Items for sale!
-                    </a>
-                  </Card.Content>
-                </Card>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
+        <Grid container verticalAlign='middle' style={gridStyle}>
+            <Container>
+              <style>{'body { background-color: #3aafa9; }'}</style>
+              <Grid verticalAlign='middle' className='background-color' columns={4}>
+
+                <Grid.Row centered>
+                  <Grid.Column width={6}>
+                    <Image size='medium' rounded floated='left' src='/images/tommy-boy.jpg'/>
+                  </Grid.Column>
+
+                  <Grid.Column width={8}>
+                    <Card floated='right' fluid>
+                      <Card.Content>
+                        <Card.Header>Tommy</Card.Header>
+                        <Card.Meta style={cardStyle}>
+                          Joined in 2016
+                        </Card.Meta>
+                        <Card.Description style={cardStyle}>
+                          Tommy is a comedian living in Nashville.
+                        </Card.Description>
+                      </Card.Content>
+                      <Card.Content extra>
+                        <a>
+                          <Icon name='gem'/>
+                          10 Items for sale!
+                        </a>
+                      </Card.Content>
+                    </Card>
+                  </Grid.Column>
+                </Grid.Row>
+
+              </Grid>
+            </Container>
+        </Grid>
     );
   }
 }
