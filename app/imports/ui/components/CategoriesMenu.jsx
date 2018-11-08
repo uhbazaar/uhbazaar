9714d9decf298db8a1888a333f3567614c95ee40
zakgilbert <gilbertz@hawaii.edu>
2018-11-08 13:58:20 -1000

    defaultItems and defaultCategories added to .json data set

--- app/imports/ui/components/CategoriesMenu.jsx
+++ app/imports/ui/components/CategoriesMenu.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, Icon, Grid } from 'semantic-ui-react';
 
-export default class CategoryMenu extends React.Component {
+export default class CategoriesMenu extends React.Component {
   render() {
     /*
     const categoryCards = [
