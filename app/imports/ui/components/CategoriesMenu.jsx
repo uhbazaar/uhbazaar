e746204ff25acda2f385a4b015411d9f8ea3895f
zakgilbert <gilbertz@hawaii.edu>
2018-11-08 15:50:07 -1000

    in the process of integrating the defaultCategories data into a card component and the categories page

--- app/imports/ui/components/CategoriesMenu.jsx
+++ app/imports/ui/components/CategoriesMenu.jsx
@@ -1,49 +1,28 @@
 import React from 'react';
 import { Card, Icon, Grid } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
-export default class CategoriesMenu extends React.Component {
+
+class CategoriesMenu extends React.Component {
   render() {
     const gridStyle = { marginTop: '64px' };
     return (
         <Grid className='ui link cards' verticalAlign='middle' style={gridStyle}>
-        <Card.Group centered>
-        <Card>
-          <Card.Content >
-            <Card.Header> <Icon size='big' name='book'/>Books</Card.Header>
-          </Card.Content>
-        </Card>
-          <Card>
-            <Card.Content>
-              <Card.Header> <Icon size='big' name='bed'/>Furniture</Card.Header>
-            </Card.Content>
-          </Card>
-          <Card>
-            <Card.Content>
-              <Card.Header> <Icon size='big' name='truck'/>Vehicles</Card.Header>
-            </Card.Content>
-          </Card>
-          <Card>
-            <Card.Content>
-              <Card.Header> <Icon size='big' name='heart'/>Personals</Card.Header>
-            </Card.Content>
-          </Card>
-          <Card>
-            <Card.Content>
-              <Card.Header> <Icon size='big' name='circle'/>Free</Card.Header>
-            </Card.Content>
-          </Card>
-          <Card>
-            <Card.Content>
-              <Card.Header> <Icon size='big' name='home'/>Housing</Card.Header>
-            </Card.Content>
-          </Card>
-          <Card>
-            <Card.Content>
-              <Card.Header> <Icon size='big' name='sticker mule'/>Pets</Card.Header>
-            </Card.Content>
-          </Card>
-        </Card.Group>
+          <Card.Group centered>
+            <Card>
+              <Card.Content >
+                <Card.Header> <Icon size='big' name={this.props.category.icon}/>{this.props.category.name}</Card.Header>
+              </Card.Content>
+            </Card>
+          </Card.Group>
         </Grid>
     );
   }
 }
+
+CategoriesMenu.propTypes = {
+  category: PropTypes.object.isRequired,
+};
+
+export default withRouter(CategoriesMenu);
