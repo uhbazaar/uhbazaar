0b8fb8cc50fbd9b705bf8288af6ddfe13fe2ed06
Wyatt Hoodes <hoodesw@hawaii.edu>
2018-11-12 11:36:48 -1000

    Disable max-len from eslint for image url

--- app/imports/ui/pages/UHBazaar.jsx
+++ app/imports/ui/pages/UHBazaar.jsx
@@ -5,8 +5,8 @@ export default class UHBazaar extends React.Component {
   render() {
     return (
         <div>
-          <style>
-            {'body { background: url(\'https://farm2.staticflickr.com/1228/1273406911_5023b91baa_b.jpg\') no-repeat center center fixed; }'}</style>
+          {/* eslint-disable-next-line max-len */}
+          <style>{'body { background: url(\'https://farm2.staticflickr.com/1228/1273406911_5023b91baa_b.jpg\') no-repeat center center fixed; }'}</style>
           <style>
             {'body { background-size: cover; }'}
           </style>
