d99bcf8f6518e7ee7478835eff24b02c97508a4a
Wyatt Hoodes <hoodesw@hawaii.edu>
2018-11-11 23:12:01 -1000

    Fixed the landing page, with scroll feature

--- app/imports/ui/components/FullWidthImage.jsx
+++ app/imports/ui/components/FullWidthImage.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import { Image, Container } from 'semantic-ui-react';
+import { Header, Container, Grid, Icon } from 'semantic-ui-react';
 
 export default class FullWidthImage extends React.Component {
   render() {
+    const containerStyle = { marginTop: '400px', marginBottom: '64vh' };
+    const containerStyle2 = {
+      backgroundColor: '#feffff',
+      opacity: '0.7',
+      width: '100%',
+      bottom: '0',
+      borderRadius: '20px',
+    };
+    const headerStyle = { fontFamily: 'Baven' };
+    const headerStyle2 = { marginTop: '512px', fontFamily: 'Baven' };
     return (
-        <Container fluid>
-          <Image src='https://farm2.staticflickr.com/1228/1273406911_5023b91baa_b.jpg' fluid/>
-          <p className="landing-welcome">
-            <b>Welcome!</b>
-          </p>
+        <Container fluid style={containerStyle}>
+          <Grid verticalAlign='middle'>
+            <Container centered>
+
+              <Header style={headerStyle} textAlign='center' size='huge'>
+                A Marketplace for Students, By Students
+              </Header>
+              <Header style={headerStyle} textAlign='center' as='h3'>
+                Welcome to a place to buy, sell, or exchange
+              </Header>
+              <Header style={headerStyle} textAlign='center' as='h3'>
+                Pass down, trade up, avoid getting burned
+              </Header>
+
+              <Container fluid style={containerStyle2}>
+                <Header style={headerStyle2} as='h2' icon textAlign='center'>
+                  <Icon name='users' circular/>
+                  <Header.Content>A community for the community</Header.Content>
+                </Header>
+              </Container>
+
+            </Container>
+          </Grid>
         </Container>
     );
   }
--- app/imports/ui/pages/UHBazaar.jsx
+++ app/imports/ui/pages/UHBazaar.jsx
@@ -3,8 +3,15 @@ import FullWidthImage from '../components/FullWidthImage';
 
 export default class UHBazaar extends React.Component {
   render() {
+    const cardColor = { backgroundColor: '#feffff' };
+    const cardFontStyle = { color: '#17252a' };
     return (
         <div>
+          <style>
+            {'body { background: url(\'https://farm2.staticflickr.com/1228/1273406911_5023b91baa_b.jpg\') no-repeat center center fixed; }'}</style>
+          <style>
+            {'body { background-size: cover; }'}
+          </style>
           <FullWidthImage/>
         </div>
     );
