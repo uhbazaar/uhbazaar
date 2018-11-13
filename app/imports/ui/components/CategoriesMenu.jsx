e68b248af9aab7a5de380127f48e59e2d89d3eae
zakgilbert <gilbertz@hawaii.edu>
2018-11-13 11:28:43 -1000

    fixed <a> bug in categories

--- app/imports/ui/components/CategoriesMenu.jsx
+++ app/imports/ui/components/CategoriesMenu.jsx
@@ -12,10 +12,8 @@ class CategoriesMenu extends React.Component {
                   <Icon size='big' name={this.props.category.icon}/> {`  ${this.props.category.name}`}</Card.Header>
               </Card.Content>
               <Card.Content extra>
-                <a>
                   <Icon name='list' />
                   Number of Listings
-                </a>
               </Card.Content>
             </Card>
     );
