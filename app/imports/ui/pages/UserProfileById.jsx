4744b8c17e76b5d2b9610e7d80ebb5c60e015963
Wyatt Hoodes <hoodesw@hawaii.edu>
2018-12-05 23:12:41 -1000

    Rating has lots of bugs

--- app/imports/ui/pages/UserProfileById.jsx
+++ app/imports/ui/pages/UserProfileById.jsx
@@ -5,19 +5,27 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { size, sortBy } from 'underscore';
+import { Bert } from 'meteor/themeteorchef:bert';
 import ShowcaseItem from '../components/ShowcaseItem';
 import { Items } from '../../api/item/item';
+import { Ratings } from '../../api/rating/rating';
 
 /** Renders the Page for editing a single document. */
+
 class UserProfileById extends React.Component {
   constructor(props) {
     super(props);
+    this.rating = this.rating;
     this.state = {
-      rateCount: 1,
-      ratingSum: 3,
+      rating: 4,
+      maxRating: 5,
       disabled: false,
     };
   }
+
+  handleRate = (e, { rating, maxRating }) => this.setState({ rating, maxRating },
+      console.log(this.state.rating),
+      this.setData())
 
   getItems(items, owner) {
     const stuff = sortBy(items, 'owner');
@@ -32,11 +40,21 @@ class UserProfileById extends React.Component {
     return size(total);
   }
 
-  handleRate = (e, { rating }) => this.setState({
-    ratingSum: this.state.ratingSum + rating,
-    rateCount: this.state.rateCount + 1,
-    disabled: true,
-  })
+  setData() {
+    const myRatings = Ratings.find({ owner: this.props.doc.username }).fetch();
+    if (this.state.rating !== 0) {
+      const owner = this.props.doc.username;
+      const ratingSum = myRatings[0].ratingSum + this.state.rating;
+      const ratingCount = myRatings[0].ratingCount + 1;
+      const _id = myRatings[0]._id;
+
+      Ratings.update({ _id: _id }, { $set: { owner, ratingSum, ratingCount } }, (error) => (error ?
+          Bert.alert({ type: 'danger', message: `Update failed: ${error.message}` }) :
+          Bert.alert({ type: 'success', message: 'Update succeeded' })));
+      this.setState({ disabled: true });
+    }
+    console.log(myRatings);
+  }
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -57,8 +75,7 @@ class UserProfileById extends React.Component {
         ' background-blend-mode: overlay; background-size: cover;}'}
         </style>
     );
-    console.log(this.state.rateCount);
-    console.log(this.state.ratingSum);
+    const myRatings = Ratings.find({ owner: this.props.doc.username }).fetch();
     return (
         <Grid container verticalAlign='middle' style={gridStyle}>
           {background}
@@ -84,9 +101,11 @@ class UserProfileById extends React.Component {
                       </a>
                     </Card.Content>
                     <Card.Content>
-                      <Rating rating={Math.floor(this.state.ratingSum / this.state.rateCount)} icon='star'
+                      <Rating icon='star'
                               onRate={this.handleRate}
-                              maxRating={5} disabled={this.state.disabled}/>
+                              rating={myRatings.length === 0 ? 3 : myRatings[0].ratingSum / myRatings[0].ratingCount}
+                              maxRating={5}
+                              disabled={this.state.disabled}/>
                     </Card.Content>
                   </Card>
                 </Grid.Column>
@@ -121,7 +140,9 @@ class UserProfileById extends React.Component {
 /** Require the presence of a Stuff document in the props object. Uniforms adds 'model' to the props, which we use. */
 UserProfileById.propTypes = {
   doc: PropTypes.object,
+  ratings: PropTypes.array,
   ready: PropTypes.bool.isRequired,
+  ready2: PropTypes.bool.isRequired,
   items: PropTypes.array.isRequired,
 };
 
@@ -131,9 +152,12 @@ export default withTracker(({ match }) => {
   const documentId = match.params._id;
   // Get access to Stuff documents.
   const subscription = Meteor.subscribe('UserSearch');
+  const subscription2 = Meteor.subscribe('Ratings');
   return {
     items: Items.find({}).fetch(),
     doc: Users.findOne(documentId),
+    ratings: Ratings.find({}).fetch(),
     ready: subscription.ready(),
+    ready2: subscription2.ready(),
   };
 })(UserProfileById);
